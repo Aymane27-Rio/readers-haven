6faec24a22298458871dbe7500a5157fc739742c
abde14-ik <abdeikbi200@gmail.com>
2025-11-16 18:03:06 +0000

    Fixing Test Issues

--- services/quotes-service/tests/server.test.js
+++ services/quotes-service/tests/server.test.js
@@ -2,6 +2,7 @@ import { test } from 'node:test';
 import assert from 'node:assert/strict';
 import request from 'supertest';
 
+process.env.NODE_ENV = 'test';
 process.env.DISABLE_TRACING = 'true';
 
 const { app } = await import('../server.js');
